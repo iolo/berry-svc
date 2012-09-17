7e41a7fe93373b68f363b2ce2cde302ce9091724
Dongsu Jang <iolothebard@kthcorp.com>
2012-09-18 00:25:49 +0900

    mock interface and impl.

--- src/main/scala/berry.scala
+++ src/main/scala/berry.scala
@@ -1,85 +1,67 @@
 package kr.iolo.berry.svc;
  
 import akka.actor._
-import akka.routing.RoundRobinRouter
-import akka.util.Duration
+import akka.dispatch._
 import akka.util.duration._
+import org.joda.time._
  
-object Pi extends App {
- 
-  calculate(nrOfWorkers = 4, nrOfElements = 10000, nrOfMessages = 10000)
- 
-  sealed trait PiMessage
-  case object Calculate extends PiMessage
-  case class Work(start: Int, nrOfElements: Int) extends PiMessage
-  case class Result(value: Double) extends PiMessage
-  case class PiApproximation(pi: Double, duration: Duration)
- 
-  class Worker extends Actor {
- 
-    def calculatePiFor(start: Int, nrOfElements: Int): Double = {
-      var acc = 0.0
-      for (i ← start until (start + nrOfElements))
-        acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
-      acc
-    }
- 
-    def receive = {
-      case Work(start, nrOfElements) ⇒
-        sender ! Result(calculatePiFor(start, nrOfElements)) // perform the work
-    }
+class BerryEntry(val id:String, val title:String, val updated:DateTime) {
+  override def toString = "[BerryEntry: id=%s, title=%s, updated=%s]".format(id, title, updated)
+}
+
+object BerryEntry {
+  def apply(id:String, title:String, updated:DateTime) = new BerryEntry(id, title, updated)
+}
+
+trait BerryService {
+  def addEntry(entry:BerryEntry): Unit
+
+  def removeEntry(id:String): Unit
+
+  def listEntries(): Future[Iterable[BerryEntry]]
+}
+
+// in-memory impl
+class BerryServiceImpl extends BerryService {
+
+  val entries = new collection.mutable.HashMap[String,BerryEntry]
+
+  import TypedActor.dispatcher
+
+  def addEntry(entry:BerryEntry) = {
+    entries += (entry.id -> entry)
   }
- 
-  class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef)
-    extends Actor {
- 
-    var pi: Double = _
-    var nrOfResults: Int = _
-    val start: Long = System.currentTimeMillis
- 
-    val workerRouter = context.actorOf(
-      Props[Worker].withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
- 
-    def receive = {
-      case Calculate ⇒
-        for (i ← 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
-      case Result(value) ⇒
-        pi += value
-        nrOfResults += 1
-        if (nrOfResults == nrOfMessages) {
-          // Send the result to the listener
-          listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
-          // Stops this actor and all its supervised children
-          context.stop(self)
-        }
-    }
- 
+
+  def removeEntry(id:String) = {
+    entries -= id
   }
- 
-  class Listener extends Actor {
-    def receive = {
-      case PiApproximation(pi, duration) ⇒
-        println("\n\tPi approximation: \t\t%s\n\tCalculation time: \t%s"
-          .format(pi, duration))
-        context.system.shutdown()
-    }
+
+  def listEntries() = {
+    Promise.successful(entries.values)
   }
+}
+
+object BerryLocalTestApp extends App {
  
+  doLocalTest()
  
-  def calculate(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int) {
-    // Create an Akka system
-    val system = ActorSystem("PiSystem")
+  def doLocalTest() {
+    val system = ActorSystem("BerrySystem")
  
-    // create the result listener, which will print the result and shutdown the system
-    val listener = system.actorOf(Props[Listener], name = "listener")
- 
-    // create the master
-    val master = system.actorOf(Props(new Master(
-      nrOfWorkers, nrOfMessages, nrOfElements, listener)),
-      name = "master")
- 
-    // start the calculation
-    master ! Calculate
- 
+    val berry:BerryService = TypedActor(system).typedActorOf(TypedProps[BerryServiceImpl]())
+
+    berry.addEntry(BerryEntry("1", "hello", DateTime.now))
+
+    berry.addEntry(BerryEntry("2", "world", DateTime.now))
+
+    println(Await.result(berry.listEntries(), 1 seconds))
+
+    berry.removeEntry("1")
+
+    println(Await.result(berry.listEntries(), 1 seconds))
+
+    berry.removeEntry("2")
+
+    println(Await.result(berry.listEntries(), 1 seconds))
   }
 }
